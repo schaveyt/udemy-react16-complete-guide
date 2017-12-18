b1932c885777d6095b4c845cda4457afabdd2146
Todd Schavey <todd.schavey@ge.com>
2017-12-18 06:51:40 -0500

    02 - stripped down to basics skeleton to get started

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,14 @@
 import * as React from 'react';
 import './App.css';
-import { PersonCard } from './Components/Presentation/PersonCard';
-import { Person } from './Types/Person';
-
-const logo = require('./logo.svg');
 
 interface AppState {
-  people: Array<Person>;
-  showPeople: boolean;
+
 }
 
 class App extends React.Component<{}, AppState> {
-
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      people: [
-        {id: 0, name: 'Fred', age: 13},
-        {id: 1, name: 'Barney', age: 18},
-        {id: 2, name: 'Wilma', age: 22},
-      ],
-      showPeople: false
-    };
-  }
-
-  toggleShowPersonHandler = () => {
-    let copyOfState = this.state.showPeople;
-    this.setState({showPeople: !copyOfState});
-  }
-
-  onDeletePersonHandler = (personId: number) => {
-    const modifiedState = this.state.people.filter((person) => {
-      return person.id !== personId;
-    });
-    this.setState({people: modifiedState});
-  }
-
-  namedChangedHandler = (newName: string, personId: number) => {
-    const copyOfState = this.state.people.splice(0);
-    copyOfState.map((person) => {
-      if (person.id === personId) {
-        person.name = newName;
-      }
-    });
-    this.setState({people: copyOfState});
-    
-  }
-
   render() {
-    const btnStyle = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
-    let peopleElements: JSX.Element | undefined = undefined;
-
-    if (this.state.showPeople) {
-      peopleElements = (
-        <div>
-          {
-            this.state.people.map((p) => {
-              return (
-                <PersonCard 
-                  person={p} 
-                  key={p.id} 
-                  deleteClicked={this.onDeletePersonHandler} 
-                  namedChanged={this.namedChangedHandler} 
-                />);
-            })
-          }
-        </div> 
-      );
-
-      btnStyle.backgroundColor = 'red';
-    }
-
-    const styleClasses = new Array<string>();
-
-    if (this.state.people.length <= 2) {
-      styleClasses.push('red');
-    }
-
-    if (this.state.people.length <= 1) {
-      styleClasses.push('bold');
-    }
-
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <p className={styleClasses.join(' ')}>This is really working.</p>
-        </div>
-        <button style={btnStyle} onClick={this.toggleShowPersonHandler}>Toggle People</button>
-        {peopleElements}
-      </div>
+      <div/>
     );
   }
 }
